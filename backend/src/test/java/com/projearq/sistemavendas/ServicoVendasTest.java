16f6fddd0a102669eee6dabbcd426863785990c2
ggvell <gg_velloso@outlook.com>
2022-06-20 20:17:17 -0300

    testes em selenium feitos

--- backend/src/test/java/com/projearq/sistemavendas/ServicoVendasTest.java
+++ backend/src/test/java/com/projearq/sistemavendas/ServicoVendasTest.java
@@ -44,7 +44,7 @@ public class ServicoVendasTest {
     @Mock
     private ServicoDeProduto servicoDeProduto;
 
-    private static final String driverDoChrome = "D:\\Documentos\\VERVAL\\chromedriver.exe";
+    private static final String driverDoChrome = "C:\\Users\\gg_ve\\OneDrive\\Documentos\\PUCRS\\dev\\chromedriver.exe";
 
     @Test
     public void testVerificaDisponibilidadeTrue() {
@@ -175,9 +175,9 @@ public class ServicoVendasTest {
     }
 
     @Test
-    @DisplayName("Tentando abrir o projeto")
-    public void testaCompra() {
-        System.out.println("+++++ Iniciando Exemplo 1...");
+    @DisplayName("Testa valor total de Produto + Imposto")
+    public void testaValorTabelado() {
+        System.out.println("Iniciando Exemplo 1");
         // Propriedades Gerais
         System.setProperty("webdriver.chrome.driver", driverDoChrome);
 
@@ -189,19 +189,252 @@ public class ServicoVendasTest {
         final WebDriver driver = new ChromeDriver();
 
         // Abre o index do projeto
-        driver.get("file:///D:/Documentos/VERVAL/VerVal2/SistemaDeVendas/frontend/index.html");
+        driver.get("file:///C:/Users/gg_ve/OneDrive/Documentos/PUCRS/VV2/verval2-t1/frontend/index.html");
 
+        // Espera item "Geladeira"
         WebDriverWait wait1 = new WebDriverWait(driver, 10);
         wait1.until(ExpectedConditions.visibilityOfElementLocated(By.id("select0")));
 
+        // Seleciona item "Geladeira"
         WebElement produtoClickado = driver.findElement(By.id("select0"));
         produtoClickado.click();
 
+        // Seleciona valor total do carrinho
         WebElement elementoResultado = driver.findElement(By.id("txtTotal"));
         String strResult = elementoResultado.getText();
 
+        // Compara com valor tabelado
         assertEquals(strResult, "Total: R$ 2800.62");
 
         driver.quit();
     }
+
+    @Test
+    @DisplayName("Testa botão de limpar o carrinho")
+    public void testaLixeira() {
+        System.out.println("Iniciando Exemplo 2");
+        // Propriedades Gerais
+        System.setProperty("webdriver.chrome.driver", driverDoChrome);
+
+        // Configurações do Chrome
+        final ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+
+        // Instancia o WebDriver do Chrome
+        final WebDriver driver = new ChromeDriver();
+
+        // Abre o index do projeto
+        driver.get("file:///C:/Users/gg_ve/OneDrive/Documentos/PUCRS/VV2/verval2-t1/frontend/index.html");
+
+        WebDriverWait wait1 = new WebDriverWait(driver, 10);
+        wait1.until(ExpectedConditions.visibilityOfElementLocated(By.id("select0")));
+
+        // Seleciona itens "Geladeira" e "Lava louça"
+        WebElement produtoClickado = driver.findElement(By.id("select0"));
+        produtoClickado.click();
+        WebElement produtoClickado1 = driver.findElement(By.id("select2"));
+        produtoClickado1.click();
+
+        // Seleciona lixeira e limpa o carrinho
+        wait1.until(ExpectedConditions.visibilityOfElementLocated(By.id("btnClear")));
+        WebElement lixeira = driver.findElement(By.id("btnClear"));
+        lixeira.click();
+
+        // Seleciona valor total do carrinho
+        WebElement elementoResultado = driver.findElement(By.id("txtTotal"));
+        String strResult = elementoResultado.getText();
+
+        // Verifica se o carrinho ficou vazio 
+        assertEquals(strResult, "Total: R$ 0.00");
+
+        driver.quit();
+
+    }
+
+    @Test
+    @DisplayName("Testa compra de produto indisponivel")
+    public void testaIndisponivel() {
+        System.out.println("Iniciando Exemplo 3");
+        // Propriedades Gerais
+        System.setProperty("webdriver.chrome.driver", driverDoChrome);
+
+        // Configurações do Chrome
+        final ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+
+        // Instancia o WebDriver do Chrome
+        final WebDriver driver = new ChromeDriver();
+
+        // Abre o index do projeto
+        driver.get("file:///C:/Users/gg_ve/OneDrive/Documentos/PUCRS/VV2/verval2-t1/frontend/index.html");
+
+        WebDriverWait wait1 = new WebDriverWait(driver, 10);
+        wait1.until(ExpectedConditions.visibilityOfElementLocated(By.id("select0")));
+
+        // Seleciona itens "Fogao" que está indisponível
+        WebElement produtoClickado = driver.findElement(By.id("select1"));
+        produtoClickado.click();
+
+        // Recebe mensagem de erro
+        wait1.until(ExpectedConditions.visibilityOfElementLocated(By.id("swal2-title")));
+        WebElement msgErro = driver.findElement(By.id("swal2-title"));
+
+        assertEquals(msgErro.getText(), "Ops!");
+
+        driver.quit();
+    }
+
+    @Test
+    @DisplayName("Testa exceção de valor limite")
+    public void testaValorLimite() {
+        System.out.println("Iniciando Exemplo 4");
+        // Propriedades Gerais
+        System.setProperty("webdriver.chrome.driver", driverDoChrome);
+
+        // Configurações do Chrome
+        final ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+
+        // Instancia o WebDriver do Chrome
+        final WebDriver driver = new ChromeDriver();
+
+        // Abre o index do projeto
+        driver.get("file:///C:/Users/gg_ve/OneDrive/Documentos/PUCRS/VV2/verval2-t1/frontend/index.html");
+
+        WebDriverWait wait1 = new WebDriverWait(driver, 10);
+        wait1.until(ExpectedConditions.visibilityOfElementLocated(By.id("select0")));
+
+        // Seleciona iten "Geladeira" e coloca no carrinho até estourar o valor limite
+        WebElement produtoClickado = driver.findElement(By.id("select0"));
+        produtoClickado.click();
+        produtoClickado.click();
+        produtoClickado.click();
+        produtoClickado.click();
+
+        // Recebe mensagem de erro
+        wait1.until(ExpectedConditions.visibilityOfElementLocated(By.id("swal2-title")));
+        WebElement msgErro = driver.findElement(By.id("swal2-title"));
+
+        assertEquals(msgErro.getText(), "Ops!");
+
+        driver.quit();
+    }
+
+    @Test
+    @DisplayName("Testa compra simples")
+    public void testaCompra() {
+        System.out.println("Iniciando Exemplo 5");
+        // Propriedades Gerais
+        System.setProperty("webdriver.chrome.driver", driverDoChrome);
+
+        // Configurações do Chrome
+        final ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+
+        // Instancia o WebDriver do Chrome
+        final WebDriver driver = new ChromeDriver();
+
+        // Abre o index do projeto
+        driver.get("file:///C:/Users/gg_ve/OneDrive/Documentos/PUCRS/VV2/verval2-t1/frontend/index.html");
+
+        WebDriverWait wait1 = new WebDriverWait(driver, 10);
+        wait1.until(ExpectedConditions.visibilityOfElementLocated(By.id("select0")));
+
+        // Seleciona iten "Geladeira" e finaliza a compra
+        WebElement produtoClickado = driver.findElement(By.id("select0"));
+        produtoClickado.click();
+        WebElement finalizaCompra = driver.findElement(By.id("btnCheckout"));
+        finalizaCompra.click();
+
+        // Recebe de sucesso
+        wait1.until(ExpectedConditions.visibilityOfElementLocated(By.id("swal2-title")));
+        WebElement msgSucesso = driver.findElement(By.id("swal2-title"));
+
+        assertEquals(msgSucesso.getText(), "Sucesso!");
+
+        driver.quit();
+    }
+
+    @Test
+    @DisplayName("Testa exceção de quantidade de um produto")
+    public void testaQuantidadeLimite() {
+        System.out.println("Iniciando Exemplo 6");
+        // Propriedades Gerais
+        System.setProperty("webdriver.chrome.driver", driverDoChrome);
+
+        // Configurações do Chrome
+        final ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+
+        // Instancia o WebDriver do Chrome
+        final WebDriver driver = new ChromeDriver();
+
+        // Abre o index do projeto
+        driver.get("file:///C:/Users/gg_ve/OneDrive/Documentos/PUCRS/VV2/verval2-t1/frontend/index.html");
+
+        WebDriverWait wait1 = new WebDriverWait(driver, 10);
+        wait1.until(ExpectedConditions.visibilityOfElementLocated(By.id("select0")));
+
+        // Seleciona iten "Aspirador de pó" e coloca no carrinho até estourar a quantidade limite
+        WebElement produtoClickado = driver.findElement(By.id("select4"));
+        produtoClickado.click();
+        produtoClickado.click();
+        produtoClickado.click();
+        produtoClickado.click();
+        produtoClickado.click();
+        produtoClickado.click();
+        produtoClickado.click();
+        produtoClickado.click();
+        produtoClickado.click();
+        produtoClickado.click();
+        produtoClickado.click();
+
+        // Recebe mensagem de erro
+        wait1.until(ExpectedConditions.visibilityOfElementLocated(By.id("swal2-title")));
+        WebElement msgErro = driver.findElement(By.id("swal2-title"));
+
+        assertEquals(msgErro.getText(), "Ops!");
+
+        driver.quit();
+    }
+
+    @Test
+    @DisplayName("Testa compra de diversos itens")
+    public void testaCompraConjunta() {
+        System.out.println("Iniciando Exemplo 7");
+        // Propriedades Gerais
+        System.setProperty("webdriver.chrome.driver", driverDoChrome);
+
+        // Configurações do Chrome
+        final ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+
+        // Instancia o WebDriver do Chrome
+        final WebDriver driver = new ChromeDriver();
+
+        // Abre o index do projeto
+        driver.get("file:///C:/Users/gg_ve/OneDrive/Documentos/PUCRS/VV2/verval2-t1/frontend/index.html");
+
+        WebDriverWait wait1 = new WebDriverWait(driver, 10);
+        wait1.until(ExpectedConditions.visibilityOfElementLocated(By.id("select0")));
+
+        // Seleciona os itens "Geladeira", "Lava louça" e "Aspirador de pó" e finaliza a compra
+        WebElement produtoClickado = driver.findElement(By.id("select0"));
+        produtoClickado.click();
+        WebElement produtoClickado1 = driver.findElement(By.id("select2"));
+        produtoClickado1.click();
+        WebElement produtoClickado2 = driver.findElement(By.id("select4"));
+        produtoClickado2.click();
+        WebElement finalizaCompra = driver.findElement(By.id("btnCheckout"));
+        finalizaCompra.click();
+
+        // Recebe de sucesso
+        wait1.until(ExpectedConditions.visibilityOfElementLocated(By.id("swal2-title")));
+        WebElement msgSucesso = driver.findElement(By.id("swal2-title"));
+
+        assertEquals(msgSucesso.getText(), "Sucesso!");
+
+        driver.quit();
+    }
+
 }
